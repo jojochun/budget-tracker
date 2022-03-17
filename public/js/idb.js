4e74697c77d0a0b962f058081b7afb2695f39246
jochun <jkchun@protonmail.com>
2022-03-16 22:23:18 -1000

    Deploying

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,23 +5,31 @@ let db;
 const request = indexedDB.open('budget_tracker', 1);
 
 request.onupgradeneeded = function (event) {
+    //save a reference to the database
     const db = event.target.result;
+    // create an object store (table) called `new_transaction`
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
+    // when db is successfully created with its object store
     db = event.target.result;
+    // check if app is online
     if (navigator.onLine) {
         uploadNewTransaction();
     }
 }
 
 request.onerror = function (event) {
+    // log error here
     console.log(event.target.errorCode);
 }
 
+// if there's no internet connection, saveRecord
 function saveRecord(record) {
+    // open a new transaction with the database with read and write permission
     const transaction = db.transaction(['new_transaction'], 'readwrite');
+    // access the object store for `new_transaction`
     const transactionObjectStore = transaction.objectStore('new_transaction');
     transactionObjectStore.add(record);
 }
